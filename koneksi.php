b7c63fd34cfe0f14a83a7fde36ba08708ddc4523
citrasn <csoptisetianingsih@gmail.com>
2025-11-28 18:12:37 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$host     = "localhost";   
+$user     = "root";        
+$password = "";            
+$database = "db_penghasilanku";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    // echo "Koneksi berhasil"; // optional
+}
+?>
